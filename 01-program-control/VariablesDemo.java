fa2e2445705a8577c186ab880583c2f21f188f11
saipranavi1234 <your@email.com>
2026-05-20 09:36:45 +0530

    Added variables and data types programs

---
+++ 01-program-control/VariablesDemo.java
@@ -0,0 +1,15 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int age = 19;
+        double cgpa = 8.7;
+        char grade = 'A';
+        boolean isJavaEasy = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java Easy: " + isJavaEasy);
+    }
+}
